c8eb3254b49c902f2cd04986a55adaa8aefb6bcb
Andy Sit <84219113+andysit1@users.noreply.github.com>
2024-04-19 18:19:05 -0400

    Update bot.py

--- app/bot.py
+++ app/bot.py
@@ -14,16 +14,4 @@ class MyClient(steam.Client):
 
 
 
-# def tradeItems(username: str, password: str, shared_secret: str, identity_secret: str, trade_token: str or int, user_id : str or int, items_to_send: list):
-#     functionClient = MyClient(trade_token=trade_token, user_id=user_id, items_to_send=items_to_send)
-#     functionClient.run(username=username, password=password, shared_secret=shared_secret, identity_secret=identity_secret)
-
-
-# tradeItems(username="polishedsnake",
-#             password="q2cmX#Ay",
-#             shared_secret="sSaYQjQ7NzufUyEJ0ODEqyKa8gU=",
-#             identity_secret="l1s5nHt8vpzIcAySerduBeI0Bws=",
-#             trade_token="MM6cAEpF", user_id="76561198309967258",
-#             items_to_send=['Black Bandana']
-#             )
 
